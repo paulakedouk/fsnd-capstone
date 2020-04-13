b787e8e1816c3319653cac8abd55140b8db25db9
Paula Kedouk <paulakedouk@gmail.com>
2020-04-13 14:47:24 -0700

    updating

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ export AUTH0_DOMAIN='http://fsndpaula.auth0.com'
 export ALGORITHMS='RS256'
 export API_AUDIENCE='api'
 export CLIENT_ID='EHUF6nr6QpYJSMYO9BQFjl4yqTsQjZ2m'
-export DATABASE_URL='postgres://upeckqdesrexua:a307fbe3c08fb604dc57178eaef37dc32625dfe79ada4bfb64d99ef749356576@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d56k1g70q0di46'
+
 export FLASK_APP=app.py
 export FLASK_DEBUG=true
 flask run --reload
